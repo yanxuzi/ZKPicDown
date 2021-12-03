8b32921c74172953b36dbda50aa16082b3f6b072
青禾 <541267163@qq.com>
2021-12-03 11:31:29 +0800

    feat(Task 异步实现加快下载速度): Task 异步实现加快下载速度
    Task 异步实现加快下载速度

--- Program.cs
+++ Program.cs
@@ -1,132 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using AgileHttp;
 using HtmlAgilityPack;
 
-namespace PictureDownLoadProject
+namespace ZhanKuImgDownLoadProject
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // 主页
-            var hrefs = GetHref();
+            var hrefs = Utils.GetHref();
             // 详情页
-            var result = GetSrcs(hrefs);
-
-            // 所有主页
-            foreach (var dictionary in result)
+            var result = Utils.GetImgSrc(hrefs);
+            // 主页 -> 详情页
+            foreach (Dictionary<string, List<string>> dictionary in result)
             {
                 // 单个详情页
-                foreach (var itemUrl in dictionary)
+                foreach (var (key, value) in dictionary)
                 {
-                    foreach (var url in itemUrl.Value)
-                    {
-                        CopyToLocal(url, itemUrl.Key);
-                    }
+                    var tasks = value.Select(url =>
+                        Utils.CopyToLocalAsync(url, key)
+                    ).ToList();
+                    await Task.WhenAll(tasks);
+                    Console.WriteLine($"{key} 下载完成");
                 }
             }
-
-
-            Console.WriteLine("下载完成");
-        }
-
-        static void CopyToLocal(string picUrl, string title)
-        {
-            var picture = picUrl.AsHttpClient()
-                .Get()
-                .Response
-                .GetResponseStream();
-
-            const string basePath = @"E:\站酷图片\";
-
-            var path = Path.Combine(basePath, title.Replace("|", ""));
-
-            if (!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-
-            var dt = DateTime.Now;
-            var fileName = $"{dt.Year}-{dt.Month}-{dt.Day}-{dt.Hour}-{dt.Minute}-{dt.Second}.jpg";
-
-            using var fs = new FileStream(@$"{path}\{fileName}", FileMode.Create);
-            picture.CopyTo(fs);
-            fs.Flush();
-        }
-
-        /// <summary>
-        /// 图片链接
-        /// </summary>
-        /// <param name="hrefs"></param>
-        /// <returns></returns>
-        static List<Dictionary<string, List<string>>> GetSrcs(List<string> hrefs)
-        {
-            var result = new List<Dictionary<string, List<string>>>();
-            foreach (var href in hrefs)
-            {
-                var srcLists = new List<string>();
-                var content = href.AsHttpClient()
-                    .Get()
-                    .GetResponseContent();
-
-                var htmlDocument = new HtmlDocument();
-                htmlDocument.LoadHtml(content);
-                var documentNode = htmlDocument.DocumentNode;
-
-                // 网站标题作为文件夹名称
-                var title = documentNode.SelectSingleNode("//h2").InnerText.Trim().Replace(" ", "");
-
-                var htmlNodeCollection = documentNode.SelectNodes("//div[@class='photo-information-content']");
-
-                foreach (var htmlNode in htmlNodeCollection)
-                {
-                    var picUrl = htmlNode.SelectSingleNode(".//img").Attributes["src"].Value;
-                    srcLists.Add(picUrl);
-                }
-
-                // 一个主页对应一个详情页多张图片
-                var data = new Dictionary<string, List<string>>
-                {
-                    {
-                        title, srcLists
-                    }
-                };
-                result.Add(data);
-            }
-
-
-            return result;
-        }
-
-
-        /// <summary>
-        /// 外层链接
-        /// </summary>
-        /// <returns></returns>
-        static List<string> GetHref()
-        {
-            var hrefList = new List<string>();
-            var url = "https://www.zcool.com.cn/collection/ZMzc2MzczNDg=";
-
-            var html = url.AsHttpClient()
-                .Get()
-                .GetResponseContent();
-
-            var htmlDocument = new HtmlDocument();
-            htmlDocument.LoadHtml(html);
-            HtmlNodeCollection htmlNodeCollection =
-                htmlDocument.DocumentNode.SelectNodes(
-                    "//div[@class='left p-relative js-item']/div[1]/div[@class='card-img']");
-
-            foreach (HtmlNode htmlNode in htmlNodeCollection)
-            {
-                var hrefStr = htmlNode.SelectSingleNode(".//a").Attributes["href"].Value;
-                hrefList.Add(hrefStr);
-            }
-
-            return hrefList;
         }
     }
 }
---
+++ Utils.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AgileHttp;
+using HtmlAgilityPack;
+
+namespace ZhanKuImgDownLoadProject
+{
+    public static class Utils
+    {
+        public static async Task CopyToLocalAsync(string picUrl, string title)
+        {
+            var picture = (await picUrl.AsHttpClient().GetAsync())
+                .Response
+                .GetResponseStream();
+
+            const string basePath = @"E:\站酷图片\";
+
+            var path = Path.Combine(basePath, title.Replace("|", ""));
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            var fileName = Guid.NewGuid();
+            await using var fs = new FileStream(@$"{path}\{fileName}", FileMode.CreateNew, FileAccess.Write);
+            await picture.CopyToAsync(fs);
+            Console.WriteLine($"{title}-{picUrl} complete...");
+        }
+
+        /// <summary>
+        /// 图片链接
+        /// </summary>
+        /// <param name="hrefs"></param>
+        /// <returns></returns>
+        public static List<Dictionary<string, List<string>>> GetImgSrc(List<string> hrefs)
+        {
+            var result = new List<Dictionary<string, List<string>>>();
+            foreach (var href in hrefs)
+            {
+                var content = href.AsHttpClient()
+                    .Get()
+                    .GetResponseContent();
+
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(content);
+                var documentNode = htmlDocument.DocumentNode;
+
+                // 网站标题 文件夹名称
+                var title = documentNode.SelectSingleNode("//h2").InnerText.Trim().Replace(" ", "");
+
+                var htmlNodeCollection = documentNode.SelectNodes("//div[@class='photo-information-content']");
+
+                var srcLists = htmlNodeCollection
+                    .Select(htmlNode =>
+                        htmlNode.SelectSingleNode(".//img").Attributes["src"].Value)
+                    .ToList();
+
+                // 详情页 多张图片
+                var data = new Dictionary<string, List<string>>
+                {
+                    {
+                        title, srcLists
+                    }
+                };
+                result.Add(data);
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// 外层链接
+        /// </summary>
+        /// <returns></returns>
+        public static List<string> GetHref()
+        {
+            const string url = "https://www.zcool.com.cn/collection/ZMzc2MzczNDg=";
+
+            var html = url.AsHttpClient()
+                .Get()
+                .GetResponseContent();
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            HtmlNodeCollection htmlNodeCollection =
+                htmlDocument.DocumentNode.SelectNodes(
+                    "//div[@class='left p-relative js-item']/div[1]/div[@class='card-img']");
+
+            return htmlNodeCollection.Select(htmlNode =>
+                    htmlNode.SelectSingleNode(".//a").Attributes["href"].Value)
+                .ToList();
+        }
+    }
+}
